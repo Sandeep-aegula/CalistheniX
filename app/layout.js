3f29d02d05d49aec8a5c8907a79e8bdfe882b9e1
Sandeep-aegula <aegulasandeep@gmail.com>
2025-12-27 23:23:25 +0530

    Analytics Added

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from '@vercel/analytics/react';
 import Providers from '@/components/Providers'
 import "./globals.css";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +27,7 @@ export default function RootLayout({ children }) {
         <Providers>
           {children}
         </Providers>
+        <Analytics />
       </body>
     </html>
   );
